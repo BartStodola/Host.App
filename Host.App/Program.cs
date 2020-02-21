e0ad74f1b7eee1f8057f2626d566a850b3867cbd
chris.stadler <stadler.christian@web.de>
2020-02-21 11:43:55 +0100

    initial commit

---
+++ Host.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Calc;
+using Logger;
+
+
+namespace Host.App
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ILogger log = new Logger.Logger();
+            log.Info("hello world.");
+
+            ICalc calc = new Calc.Calc();
+            var r = calc.Sub(1, -2);
+            log.Warn($"result less than zero: {r}");
+
+            Console.WriteLine("press any key!");
+            Console.ReadLine();
+        }
+    }
+}
